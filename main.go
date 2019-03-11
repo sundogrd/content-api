c26f94aa04207da4335d3adadecb061879f8d8ff
breakinferno <1972952841@qq.com>
2019-03-11 21:20:56 +0800

    style: 更改提交人物

--- main.go
+++ main.go
@@ -23,7 +23,6 @@ func CORSMiddleware() gin.HandlerFunc {
 		c.Writer.Header().Set("Access-Control-Allow-Headers", "X-Requested-With, Content-Type, Origin, Authorization, Accept, Client-Security-Token, Accept-Encoding, x-access-token")
 		c.Writer.Header().Set("Access-Control-Expose-Headers", "Content-Length")
 		// c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
-
 		if c.Request.Method == "OPTIONS" {
 			c.AbortWithStatus(200)
 		} else {
@@ -37,7 +36,6 @@ func main() {
 	config.Init()
 	name := config.GetString("name")
 	port := config.GetString("port")
-
 	// 初始化默认redis db, 后面在使用的时候import "github.com/ihahoo/go-api-lib/redis" 通过redis.DB(0)调用实例
 	// 如果要初始化多个redis的db，则在这里添加，比如redis.Init(1)就建立了一个db 1的连接
 	// 如果不使用redis，则删除这里以及其它和redis相关的包引入
@@ -46,7 +44,6 @@ func main() {
 		fmt.Printf("[Main] Init Redis error: %+v", err)
 		os.Exit(1)
 	}
-
 	// 初始化数据库
 	dbClient, err := db.Init()
 	if err != nil {
@@ -76,7 +73,6 @@ func main() {
 	r.NoRoute(func(c *gin.Context) {
 		c.JSON(404, gin.H{"errcode": 404, "errmsg": "Not Found"})
 	})
-
 	fmt.Println(name + " start listening at http://localhost:" + port)
 	fmt.Printf("==> 🚀 %s listening at %s\n", name, port)
 	r.Run(":" + port)
