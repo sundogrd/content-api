116ca5f288d9a33609075f5636e905a2f71a7557
lwyj123 <liang.pearce@gmail.com>
2019-04-06 21:55:59 +0800

    chore: help

--- devops/build_docker.sh
+++ devops/build_docker.sh
@@ -4,3 +4,5 @@
 cp ./data/config/app.template.json ./data/config/app.json
 sed -i 's#\("ClientID": "\).*#\1'"$1"'",#g' "./data/config/app.json"
 sed -i 's#\("ClientSecret": "\).*#\1'"$2"'"#g' "./data/config/app.json"
+
+ip -4 route list match 0/0 | awk '{print $3 " host.docker.internal"}' >> /etc/hosts
