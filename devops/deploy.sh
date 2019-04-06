fd6f8dbc044009b3867503be7eaae74e1164195c
lwyj123 <liang.pearce@gmail.com>
2019-04-06 17:42:54 +0800

    feat: 不需要postgress

--- devops/deploy.sh
+++ devops/deploy.sh
@@ -5,4 +5,4 @@ docker pull sundogrd/content-api:$1
 if docker ps -a | grep -q sundogrd-content-api; then
     docker rm -f sundogrd-content-api
 fi
-docker run -d --name sundogrd-content-api -p 9431:8086 --link postgres:postgres sundogrd/content-api:$1
+docker run -d --name sundogrd-content-api -p 9431:8086 sundogrd/content-api:$1
