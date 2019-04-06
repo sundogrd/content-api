72601f124da704a32e332353937d361207624a8b
lwyj123 <liang.pearce@gmail.com>
2019-04-06 21:01:46 +0800

    fix: change command style

--- devops/deploy.sh
+++ devops/deploy.sh
@@ -4,8 +4,8 @@
 git pull
 
 # update the repository
-docker pull sundogrd/content-api:$1
+docker pull sundogrd/content-api:$1-$2
 if docker ps -a | grep -q sundogrd-content-api; then
     docker rm -f sundogrd-content-api
 fi
-docker run -d --name sundogrd-content-api -p 9431:8086 sundogrd/content-api:$1
+docker run -d --name sundogrd-content-api -p 9431:8086 sundogrd/content-api:$1-$2
