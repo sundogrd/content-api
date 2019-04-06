6bbd2ff4d51297d24664999c23a13eb353708f67
lwyj123 <liang.pearce@gmail.com>
2019-04-06 18:16:17 +0800

    feat: config using travis

--- devops/deploy.sh
+++ devops/deploy.sh
@@ -1,5 +1,11 @@
 #!/usr/bin/env bash
 
+
+# generate app.json using app.template.json
+cp ./data/config/app.template.json ./data/config/app.json
+sed -i '' 's#\("ClientID": "\).*#\1'"$2"'",#g' "./data/config/app.json"
+sed -i '' 's#\("ClientSecret": "\).*#\1'"$3"'"#g' "./data/config/app.json"
+
 # update the repository
 docker pull sundogrd/content-api:$1
 if docker ps -a | grep -q sundogrd-content-api; then
