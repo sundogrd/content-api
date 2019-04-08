948d5fb1cb7584b1cbfa45c06c227c945912c551
lwyj123 <liang.pearce@gmail.com>
2019-04-08 13:14:30 +0800

    feat: update deploy

--- devops/deploy.sh
+++ devops/deploy.sh
@@ -5,7 +5,7 @@ git pull
 
 # update the repository
 docker pull sundogrd/content-api:$1-$2
-if docker ps -a | grep -q sundogrd-content-api; then
-    docker rm -f sundogrd-content-api
+if docker ps -a | grep -q sundogrd/content-api; then
+    docker rm -f $(docker ps -a | grep sundogrd/content-api | awk '{print $1}')
 fi
 docker run -d --name sundogrd-content-api -p 9431:8086 sundogrd/content-api:$1-$2
